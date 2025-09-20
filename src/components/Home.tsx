c3f469bd5dc8578d00d7a0d47d8bbba86dea4c1d
Anshul Koshyari <koshyaria@gmail.com>
2025-09-20 21:48:29 +0530

    Minor css fixes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -12,10 +12,10 @@ const Home = () => {
         <div className="flex flex-col items-center text-center lg:max-w-2xl lg:mx-auto px-2 sm:px-4">
           {/* Text content section */}
           <div className="animate-fade-in-down mb-32">
-            <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-semibold text-foreground mb-10 leading-tight">
+            <h1 className="text-5xl sm:text-6xl md:text-6xl lg:text-7xl font-semibold text-foreground mb-10 leading-tight">
               <span className="hero-gradient">AI wingman</span> for every conversation
             </h1>
-            <p className="text-sm sm:text-base md:text-lg lg:text-xl text-muted-foreground max-w-3xl mb-8 font-light mx-auto px-2">
+            <p className="text-base sm:text-base md:text-lg lg:text-xl text-muted-foreground max-w-3xl mb-8 font-light mx-auto px-2">
               Hovrlay is an undetectable desktop app with real time AI that transforms how you engage in{" "}
               <span className="text-foreground font-semibold">meetings, calls and interviews.</span>
             </p>
