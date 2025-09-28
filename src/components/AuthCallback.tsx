484077cb9f581e57b9a5e1c0668c3e5ec726e629
Anshul Koshyari <anshulkoshyari@gmail.com>
2025-09-28 23:20:19 +0530

    Removed state from deeplink url for oauth

--- src/components/AuthCallback.tsx
+++ src/components/AuthCallback.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import NotFound from '@/pages/NotFound';
 
-
 const AuthCallback = () => {
   const [shouldShow404, setShouldShow404] = useState(false);
 
@@ -9,14 +8,13 @@ const AuthCallback = () => {
     const searchParams = new URLSearchParams(window.location.search);
     const error = searchParams.get('error');
     const code = searchParams.get('code');
-    const state = searchParams.get('state');
 
     if (error || !code) {
       setShouldShow404(true);
       return;
     }
 
-    const deeplinkUrl = `hovrlay://auth/callback?code=${code}${state ? `&state=${state}` : ''}`;
+    const deeplinkUrl = `hovrlay://auth/callback?code=${code}`;
     window.location.href = deeplinkUrl;
   }, []);
 
