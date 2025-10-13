c40d0248732a72b1914f79aa958f5cbbd9bbff6c
Anshul Koshyari <anshulkoshyari@gmail.com>
2025-10-14 04:43:27 +0530

    Updated macOS download link to the notarized app

--- src/utils/downloads.ts
+++ src/utils/downloads.ts
@@ -1,6 +1,6 @@
 // Hardcoded download URLs for Hovrlay releases
 export const DOWNLOAD_URLS = {
-  mac: 'https://github.com/hovrlay/hovrlay-releases/releases/download/v0.1.0-beta.2/hovrlay.0.1.0-beta.2.dmg',
+  mac: 'https://github.com/hovrlay/hovrlay-releases/releases/download/v0.1.0-beta.4/Hovrlay.dmg',
   windows: 'https://github.com/hovrlay/hovrlay-releases/releases/download/v0.1.0-beta.2/hovrlay.0.1.0-beta.2.exe',
 } as const;
 
