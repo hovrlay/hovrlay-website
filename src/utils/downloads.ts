4a9bcc58dbfb215b73986e5ff37d2e71de90c613
Anshul Koshyari <anshulkoshyari@gmail.com>
2025-10-15 03:00:38 +0530

    Using release assets served by S3 rather than hovrlay releases repo

--- src/utils/downloads.ts
+++ src/utils/downloads.ts
@@ -1,7 +1,7 @@
 // Hardcoded download URLs for Hovrlay releases
 export const DOWNLOAD_URLS = {
-  mac: 'https://github.com/hovrlay/hovrlay-releases/releases/download/v0.1.0-beta.4/Hovrlay.dmg',
-  windows: 'https://github.com/hovrlay/hovrlay-releases/releases/download/v0.1.0-beta.2/hovrlay.0.1.0-beta.2.exe',
+  mac: 'https://hovrlay-release.s3.ap-south-1.amazonaws.com/hovrlay.dmg',
+  windows: 'https://hovrlay-release.s3.ap-south-1.amazonaws.com/hovrlay.exe',
 } as const;
 
 export const handleDownload = (platform: 'mac' | 'windows') => {
